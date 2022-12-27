943d36954946c534e00a88d854f49fcc1d42c8b2
Ojonugwa Martins Onogu <onogumartins01@gmail.com>
2022-12-27 19:26:51 +0100

    Added new Images

--- src/Products.js
+++ src/Products.js
@@ -7,68 +7,68 @@ export const PRODUCTS = [
      },
      {
         id: 2,
-        productName: 'Iphone 12',
-        price: 99.00,
-        image: 'https://images.pexels.com/photos/13706809/pexels-photo-13706809.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        productName: 'Gold Frame',
+        price: 250.00,
+        image: 'https://images.pexels.com/photos/2691660/pexels-photo-2691660.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
      },
      {
         id: 3,
-        productName: 'Iphone 11',
-        price: 99.00,
-        image: 'https://images.pexels.com/photos/13706809/pexels-photo-13706809.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        productName: 'RetroTV',
+        price: 54.00,
+        image: 'https://images.pexels.com/photos/6976094/pexels-photo-6976094.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
      },
      {
         id: 4,
-        productName: 'Iphone x',
-        price: 99.00,
-        image: 'https://images.pexels.com/photos/13706809/pexels-photo-13706809.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        productName: 'Camera',
+        price: 800.00,
+        image: 'https://images.pexels.com/photos/952264/pexels-photo-952264.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
      },
      {
         id: 5,
-        productName: 'Iphone Sx',
-        price: 99.00,
-        image: 'https://images.pexels.com/photos/13706809/pexels-photo-13706809.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        productName: 'Single Chair',
+        price: 28.00,
+        image: 'https://images.pexels.com/photos/13630279/pexels-photo-13630279.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
      },
      {
         id: 6,
-        productName: 'Iphone SXmax',
-        price: 99.00,
-        image: 'https://images.pexels.com/photos/13706809/pexels-photo-13706809.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        productName: 'Wooden Utensils',
+        price: 77.00,
+        image: 'https://images.pexels.com/photos/2147637/pexels-photo-2147637.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
      },
      {
         id: 7,
-        productName: 'Iphone 8',
-        price: 99.00,
-        image: 'https://images.pexels.com/photos/13706809/pexels-photo-13706809.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        productName: 'Retro Phone',
+        price: 400.00,
+        image: 'https://images.pexels.com/photos/2111759/pexels-photo-2111759.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
      },
      {
         id: 8,
-        productName: 'Iphone 8+',
-        price: 99.00,
-        image: 'https://images.pexels.com/photos/13706809/pexels-photo-13706809.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        productName: 'Wall Mirror',
+        price: 1000.00,
+        image: 'https://images.pexels.com/photos/1528975/pexels-photo-1528975.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
      },
      {
         id: 9,
-        productName: 'Iphone 7',
-        price: 99.00,
-        image: 'https://images.pexels.com/photos/13706809/pexels-photo-13706809.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        productName: 'Old Lamp',
+        price: 128.00,
+        image: 'https://images.pexels.com/photos/943150/pexels-photo-943150.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
      },
      {
         id: 10,
-        productName: 'Iphone 7+',
-        price: 99.00,
-        image: 'https://images.pexels.com/photos/13706809/pexels-photo-13706809.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        productName: 'Dumbbell',
+        price: 73.00,
+        image: 'https://images.pexels.com/photos/4793221/pexels-photo-4793221.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
      },
      {
         id: 11,
-        productName: 'Iphone 6',
-        price: 99.00,
-        image: 'https://images.pexels.com/photos/13706809/pexels-photo-13706809.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        productName: 'Earring',
+        price: 12.00,
+        image: 'https://images.pexels.com/photos/12144805/pexels-photo-12144805.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
      },
      {
         id: 12,
-        productName: 'Iphone 6+',
-        price: 99.00,
-        image: 'https://images.pexels.com/photos/13706809/pexels-photo-13706809.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        productName: 'Mixer',
+        price: 157.00,
+        image: 'https://images.pexels.com/photos/3951664/pexels-photo-3951664.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
      },
 ]
